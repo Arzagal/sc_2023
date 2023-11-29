9299286e8d9fe23cf3274df80beca5a2dde7a642
Louis Fay <103536600+Arzagal@users.noreply.github.com>
2023-11-29 12:49:27 +0200

    Delete sc_accu directory

--- sc_accu/display.cpp
+++
@@ -1,9 +0,0 @@
-#include <systemc.h>
-#include "display.h"
-
-void display::affiche()
-{
-  cout << "Display : " << data_in << " " << data_sum
-       << " at time " << sc_time_stamp() << endl;
-}
-// EOF
